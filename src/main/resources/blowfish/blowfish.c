80d42f9ca09c3f44aea907cc78790c64fe7c6748
skurski <peterskurski@gmail.com>
2017-05-29 22:25:12 +0200

    Blowfish in ECB mode refactor

--- src/main/resources/blowfish/blowfish.c
+++ src/main/resources/blowfish/blowfish.c
@@ -1,164 +1,256 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <openssl/blowfish.h>
 
-#define IVARG   1
-#define KEYARG  2
-#define SRCDATA 3
-#define ARGS    4
+// block size for blowfish is 64 bits (8 bytes)
+#define BLOCK_SIZE 8
 
-#define TOKENS "\r \n"
+#define ENCRYPT_ECB_MODE 1
+#define ENCRYPT_CBC_MODE 2
+#define DECRYPT_ECB_MODE 3
+#define DECRYPT_CBC_MODE 4
 
+#define ENCRYPT "encrypt"
+#define DECRYPT "decrypt"
+#define ECB "ecb"
+#define CBC "cbc"
 
-char * readFile (char * filename);
-char * encrypt (char * source);
-void decrypt (char * source);
+#define MY_KEY "1dqbfj4hgnr66534kjgrfs"
 
-int main(int argc, char **argv) {
-    char * source = readFile("test.txt");
-    char * cipher = encrypt(source);
-    decrypt(cipher);
-}
+void encryptECB(const char * inFileName, const char * outFileName);
+void decryptECB(const char * inFileName, const char * outFileName);
+char * removePadding(char * block);
 
-char * encrypt (char * source) {
-    BF_KEY key;
-    char  inputz[strlen(source)];
-//    char outputz[1024*1024];
-
-    char * outputz = malloc(strlen(source));
-
-    char ivec[8];
-    char block[8];
-    int i;
-    int offset = 0;
-
-    char * IV = "MY*IV000";
-    char * mykey = "qazwsxedcrfvtgbyhnujmiklop";
-
-    strncpy(ivec, IV, 8);
-
-    for(i=0; i<strlen(source); i++) {
-        inputz[i] = source[i];
-    }
-
-    BF_set_key(&key, strlen(mykey), mykey);
-    while(1) {
-        for(i=0; i<8; i++)
-            block[i] = 0;
-
-        strncpy(block, inputz+offset, 8);
-
-        BF_cbc_encrypt(inputz+offset, outputz, 8, &key, ivec, BF_ENCRYPT);
-
-        for(i=0; i<strlen(outputz); i++)
-            printf("%02x ", (unsigned char) outputz[i]);
-
-        if( strlen(inputz+offset)>8 ) {
-            offset += 8;
-
-        } else {
-            break;
-        }
-    }
-
-    printf("\n");
-
-    decrypt(outputz);
-
-    return outputz;
-}
-
-void decrypt (char * source) {
-    char inputz[1024*1024];
-    char outputz[1024*1024];
-    char ivec[8];
-    char byte;
-    char *tok;
-    int pos = 0;
-    char block[8];
-    int offset;
-
-
-    BF_KEY key;
-
-    char * IV = "MY*IV000";
-    char * mykey = "qazwsxedcrfvtgbyhnujmiklop";
-
-    strncpy(ivec, IV, 8);
-
-//    tok = strtok(source, TOKENS);
-//    while( tok ) {
-//        if( sscanf(tok, "%c", &byte) ) {
-//            inputz[pos] = byte;
-//            inputz[++pos] = 0;
-//        }
-//
-//        tok = strtok(NULL, TOKENS);
+/*
+ * Encryption / Decryption using Blowfish in ECB/CBC mode
+ * Padding : PKCS7
+ * Compilation: gcc blowfish.c -o blowfish -lcrypto
+ * Execution format:
+ * ./blowfish encrypt/decrypt ecb/cbc inFileName outFileName
+ */
+int main(int argc, char * argv[]) {
+    // initializing testing values - to delete
+//    argv[0] = "main";
+//    // argv[1] = "encrypt";
+//    argv[1] = "decrypt";
+//    argv[2] = "ecb";
+//    // argv[3] = "myfile.txt";
+//    argv[3] = "outfile.txt";
+//    // argv[4] = "outfile.txt";
+//    argv[4] = "decrypt.txt";
+//    int k;
+//    for (k=0; k<5; k++) {
+//        printf("%s\n", argv[k]);
 //    }
-//
-//    BF_set_key(&key, strlen(mykey), mykey);
-//    BF_cbc_encrypt(inputz, outputz, pos, &key, ivec, BF_DECRYPT);
+    // end of testing initialization - to delete
 
-    int i;
-    for(i=0; i<strlen(source); i++) {
-        inputz[i] = source[i];
+    // program arguments validation
+    if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL || argv[4] == NULL) {
+        printf("Upss, something is wrong, check your parameters!\n");
+        printf("Correct format: encrypt/decrypt ecb/cbc inFileName outFileName\n");
+        return 1;
     }
 
-    BF_set_key(&key, strlen(mykey), mykey);
-    while(1) {
-        for(i=0; i<8; i++)
-            block[i] = 0;
-
-        strncpy(block, inputz+offset, 8);
-
-        BF_cbc_encrypt(inputz, outputz, pos, &key, ivec, BF_DECRYPT);
-
-        if( strlen(inputz+offset)>8 ) {
-            offset += 8;
-
-        } else {
-            break;
+    // choose correct mode for encryption/decryption
+    int mode;
+    if (strcmp(argv[1], ENCRYPT) == 0) {
+        if (strcmp(argv[2], ECB) == 0) {
+            mode = ENCRYPT_ECB_MODE;
+        } else  if (strcmp(argv[2], CBC) == 0) {
+            mode = ENCRYPT_CBC_MODE;
+        }
+    } else if (strcmp(argv[1], DECRYPT) == 0) {
+        if (strcmp(argv[2], ECB) == 0) {
+            mode = DECRYPT_ECB_MODE;
+        } else if (strcmp(argv[2], CBC) == 0) {
+            mode = DECRYPT_CBC_MODE;
         }
     }
 
+    char * inFile = argv[3];
+    char * outFile = argv[4];
 
+    // fire encryption/decryption in choosen mode
+    if (mode == ENCRYPT_ECB_MODE) {
+        printf("Start encrypting in ECB mode...\n");
+        encryptECB(inFile, outFile);
+        return 0;
+    }
 
+    if (mode == ENCRYPT_CBC_MODE) {
+        printf("Start encrypting in CBC mode...\n");
+        // encrypt(inFile, outFile);
+        return 0;
+    }
 
+    if (mode == DECRYPT_ECB_MODE) {
+        printf("Start decrypting in ECB mode...\n");
+        decryptECB(inFile, outFile);
+        return 0;
+    }
 
-    printf("%s\n", outputz);
+    if (mode == DECRYPT_CBC_MODE) {
+        printf("Start decrypting in CBC mode...\n");
+        // encrypt(inFile, outFile);
+        return 0;
+    }
+
+    return 0;
 }
 
-char * readFile (char * filename) {
-	FILE *file;
-	char *buffer;
-	unsigned long fileLen;
+void encryptECB(const char * inFileName, const char * outFileName) {
+    BF_KEY key;
+    FILE * inFile;
+    FILE * outFile;
+    unsigned char inBuffer[BLOCK_SIZE];
+    unsigned char outBuffer[BLOCK_SIZE];
+    int readBytes;
 
-	//Open file
-	file = fopen(filename, "rb");
-	if (!file)
-	{
-		fprintf(stderr, "Unable to open file %s", filename);
-		exit(1);
-	}
+    if ((inFile = fopen(inFileName, "r")) == NULL) {
+        printf("Open file error occurred");
+        exit(1);
+    }
 
-	//Get file length
-	fseek(file, 0, SEEK_END);
-	fileLen=ftell(file);
-	fseek(file, 0, SEEK_SET);
+    if ((outFile = fopen(outFileName, "w")) == NULL) {
+        printf("Open file error occurred");
+        exit(1);
+    }
 
-	//Allocate memory
-	buffer=(char *)malloc(fileLen+1);
-	if (!buffer)
-	{
-		fprintf(stderr, "Memory error!");
-        fclose(file);
-		exit(1);
-	}
+    // set key before encryption
+    BF_set_key(&key, strlen(MY_KEY), MY_KEY);
 
-	//Read file contents into buffer
-	fread(buffer, fileLen, 1, file);
-	fclose(file);
+    while (1) {
+        // reset buffer
+        memset(inBuffer, 0, BLOCK_SIZE);
 
-    return buffer;
+        // read 8 bytes of data in each loop
+        if ((readBytes = fread(inBuffer, 1, BLOCK_SIZE, inFile)) == -1) {
+            printf("Read error occurred");
+        }
+
+        // end of file
+        if (readBytes == 0) {
+            break;
+        }
+
+        // apply padding if needed
+        if (readBytes < BLOCK_SIZE) {
+            printf("Number of bytes read: %i", readBytes);
+            unsigned char padding = BLOCK_SIZE - readBytes;
+            printf(" -> padding aplied: %i -> ", padding);
+            int j = readBytes;
+            for (j; j < BLOCK_SIZE; j++) {
+                inBuffer[j] = padding;
+            }
+            readBytes = readBytes + padding;
+        }
+
+        printf("Number of bytes read: %i\n", readBytes);
+
+        // do encryption
+        BF_ecb_encrypt(inBuffer, outBuffer, &key, BF_ENCRYPT);
+
+        printf("Hex representation: ");
+        int x;
+        for (x=0; x<readBytes; x++) {
+            printf("%02x ", (unsigned char) outBuffer[x]);
+        }
+        printf("\n");
+
+        // save encrypted part of file
+        fwrite(outBuffer, 1, BLOCK_SIZE, outFile);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+}
+
+void decryptECB(const char * inFileName, const char * outFileName) {
+    BF_KEY key;
+    FILE * inFile;
+    FILE * outFile;
+    unsigned char inBuffer[BLOCK_SIZE];
+    unsigned char outBuffer[BLOCK_SIZE];
+    int readBytes;
+    int notFirstIteration = 0;
+
+    if ((inFile = fopen(inFileName, "r")) == NULL) {
+        printf("Open file error occurred");
+        exit(1);
+    }
+
+    if ((outFile = fopen(outFileName, "w")) == NULL) {
+        printf("Open file error occurred");
+        exit(1);
+    }
+
+    // set key before encryption
+    BF_set_key(&key, strlen(MY_KEY), MY_KEY);
+
+    while (1) {
+        // reset buffer
+        memset(inBuffer, 0, BLOCK_SIZE);
+
+        // read 8 bytes of data in each loop
+        if ((readBytes = fread(inBuffer, 1, BLOCK_SIZE, inFile)) == -1) {
+            printf("Read error occurred");
+        }
+
+        // end of file
+        if (readBytes == 0) {
+            // save decrypted block from previous iteration
+            // if the previous iteration was last remove padding before saving
+            char * originalBlock = removePadding(outBuffer);
+            fwrite(originalBlock, 1, strlen(originalBlock), outFile);
+            break;
+        }
+
+        if (notFirstIteration) {
+            // save decrypted block from previous iteration         
+            fwrite(outBuffer, 1, BLOCK_SIZE, outFile);
+        }
+
+        // do decryption
+        BF_ecb_encrypt(inBuffer, outBuffer, &key, BF_DECRYPT);
+
+        printf("Hex representation: ");
+        int x;
+        for (x=0; x<readBytes; x++) {
+            printf("%02x ", (unsigned char) outBuffer[x]);
+        }
+        printf("\n");
+
+        notFirstIteration =1; // set the flag
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+}
+
+char * removePadding(char * buffer) {
+    // check if last char represent padding
+    if (buffer[BLOCK_SIZE-1] >= 1 && buffer[BLOCK_SIZE-1] <= 8) {
+        printf("Padding value of last block: %02x\n" , buffer[BLOCK_SIZE-1]);
+        char * block;
+        int size = BLOCK_SIZE - buffer[BLOCK_SIZE-1];
+
+        // blocksize - padding value
+        block = malloc(size * sizeof(char));
+        printf("Size of block after padding removal: %i\n" , size);
+
+        printf("Hex representation of last block: ");
+        int i;
+        for (i = 0; i < size; i++) {
+            block[i] = buffer[i];
+            printf("%02x ", (unsigned char) block[i]);
+        }
+        printf("\n");
+
+        return block;
+    }
+
+    printf("No padding\n");
+    return (char *) buffer;
 }
